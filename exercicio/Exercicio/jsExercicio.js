95fc58f4db2757ee2b3156f874be1249eded0c4b
USE104 <jose.hssilva4@senacsp.edu.br>
2022-08-24 11:37:08 -0300

    Javascript WEB

---
+++ exercicio/Exercicio/jsExercicio.js
@@ -0,0 +1,11 @@
+function clicar(){
+    document.querySelector('.box').innerText = 'Clicou'
+}
+
+function mouseIn(){
+    document.querySelector('.box').innerText = 'Passou o mouse'
+}
+
+function mouseOut(){
+    document.querySelector('.box').innerText = 'Tirou o mouse'
+}
